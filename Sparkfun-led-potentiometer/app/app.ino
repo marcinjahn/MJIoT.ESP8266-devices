402a1a9578ccba369dc0b447535ae476edb4baa6
Marcin <marcinj10@gmail.com>
2018-07-05 19:03:25 +0200

    Add LED-Potentiometer device project

---
+++ Sparkfun-led-potentiometer/app/app.ino
@@ -0,0 +1,135 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// Please use an Arduino IDE 1.6.8 or greater
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WiFiUdp.h>
+
+#include <AzureIoTHub.h>
+#include <AzureIoTProtocol_MQTT.h>
+#include <AzureIoTUtility.h>
+
+#include "config.h"
+
+static bool messagePending = false;
+static bool messageSending = true;
+
+static char *connectionString;
+static char *ssid;
+static char *pass;
+
+
+//MAIN FUNCTIONALITY:
+void setupDevice() {
+
+  clearParam();
+  
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
+
+    initSerial();
+    delay(4000);
+    //initializeWifiCredentials();
+
+    ssid = "Redmi4X";
+    pass = "homologacja";
+    connectionString = "HostName=MJIoT-Hub.azure-devices.net;DeviceId=41;SharedAccessKey=oAdJS4MWtFfIntX2fw/sXXv7ldpsLsF2Vgeke1R+X3o=";
+    initWifi();
+    initTime();
+}
+
+
+
+
+//UTILITIES:
+void initWifi()
+{
+    // Attempt to connect to Wifi network:
+    Serial.printf("Attempting to connect to SSID: %s.\r\n", ssid);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        // Get Mac Address and show it.
+        // WiFi.macAddress(mac) save the mac address into a six length array, but the endian may be different. The huzzah board should
+        // start from mac[0] to mac[5], but some other kinds of board run in the oppsite direction.
+        uint8_t mac[6];
+        WiFi.macAddress(mac);
+        Serial.printf("You device with MAC address %02x:%02x:%02x:%02x:%02x:%02x connects to %s failed! Waiting 10 seconds to retry.\r\n",
+                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], ssid);
+        WiFi.begin(ssid, pass);
+        delay(10000);
+    }
+    Serial.printf("Connected to wifi %s.\r\n", ssid);
+}
+
+void initTime()
+{
+    time_t epochTime;
+    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+    while (true)
+    {
+        epochTime = time(NULL);
+
+        if (epochTime == 0)
+        {
+            Serial.println("Fetching NTP epoch time failed! Waiting 2 seconds to retry.");
+            delay(2000);
+        }
+        else
+        {
+            Serial.printf("Fetched NTP epoch time is: %lu.\r\n", epochTime);
+            break;
+        }
+    }
+}
+
+
+int lastPotentiometerReading = 0;
+void handlePotentiometer() {
+  static int number = 0;
+  number++;
+
+  if (number < 100)
+    return;
+    
+  number = 0;
+  
+  int newReading = analogRead(A0);
+  if (lastPotentiometerReading != newReading) {
+    lastPotentiometerReading = newReading;
+    SendStateToCloud(false);
+  }
+}
+
+
+static IOTHUB_CLIENT_LL_HANDLE iotHubClientHandle;
+void setup()
+{
+    setupDevice();
+    
+
+    iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString(connectionString, MQTT_Protocol);
+    if (iotHubClientHandle == NULL)
+    {
+        Serial.println("Failed on IoTHubClient_CreateFromConnectionString.");
+        while (1);
+    }
+
+    IoTHubClient_LL_SetMessageCallback(iotHubClientHandle, receiveMessageCallback, NULL);
+    IoTHubClient_LL_SetDeviceMethodCallback(iotHubClientHandle, deviceMethodCallback, NULL);
+//    IoTHubClient_LL_SetDeviceTwinCallback(iotHubClientHandle, twinCallback, NULL);
+}
+
+//static int messageCount = 1;
+void loop()
+{
+
+    IoTHubClient_LL_DoWork(iotHubClientHandle);
+    delay(10);
+    handlePotentiometer();
+}
---
+++ Sparkfun-led-potentiometer/app/message.ino
@@ -0,0 +1,19 @@
+#include <ArduinoJson.h>
+
+bool createMessage(char *payload, bool isLedMessage)
+{
+    StaticJsonBuffer<MESSAGE_MAX_LEN> jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+    
+    root["DeviceId"] = DEVICE_ID;
+    if (isLedMessage) {
+      root["PropertyName"] = "LED State";
+      root["PropertyValue"] = digitalRead(LED_PIN) ? "false" : "true";
+    }
+    else {
+      root["PropertyName"] = "Potentiometer Value";
+      root["PropertyValue"] = analogRead(A0);
+    }
+    
+    root.printTo(payload, MESSAGE_MAX_LEN);
+}
