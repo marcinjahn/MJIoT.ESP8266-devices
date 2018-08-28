33f3c075cd48e851822d4e8e3e291c0265dad904
Marcin <marcinj10@gmail.com>
2018-08-28 18:43:54 +0200

    Add thermometer device

---
+++ Sparkfun-dht22/app/config.h
@@ -0,0 +1,18 @@
+// Physical device information for board and sensor
+#define DEVICE_ID "43"
+
+#define DHT_TYPE DHT22
+
+// Pin layout configuration
+#define DHT_PIN 2
+
+// EEPROM address configuration
+#define EEPROM_SIZE 512
+
+// SSID and SSID password's length should < 32 bytes
+// http://serverfault.com/a/45509
+#define SSID_LEN 32
+#define PASS_LEN 32
+#define CONNECTION_STRING_LEN 256
+
+#define MESSAGE_MAX_LEN 256
---
+++ Sparkfun-dht22/app/message.ino
@@ -0,0 +1,47 @@
+#include <Adafruit_Sensor.h>
+#include <ArduinoJson.h>
+#include <DHT.h>
+
+static DHT dht(DHT_PIN, DHT_TYPE);
+void initSensor()
+{
+    dht.begin();
+}
+
+float readTemperature()
+{
+    return dht.readTemperature();
+}
+
+float readHumidity()
+{
+    return dht.readHumidity();
+}
+
+
+
+void createTemperatureMessage(char *payload)
+{
+    float temperature = readTemperature();
+    StaticJsonBuffer<MESSAGE_MAX_LEN> jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+    
+    root["DeviceId"] = DEVICE_ID;
+    root["PropertyName"] = "Temperature";
+    root["PropertyValue"] = temperature;
+    
+    root.printTo(payload, MESSAGE_MAX_LEN);
+}
+
+void createHumidityMessage(char *payload)
+{
+    float humidity = readHumidity();
+    StaticJsonBuffer<MESSAGE_MAX_LEN> jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+    
+    root["DeviceId"] = DEVICE_ID;
+    root["PropertyName"] = "Humidity";
+    root["PropertyValue"] = humidity;
+    
+    root.printTo(payload, MESSAGE_MAX_LEN);
+}
