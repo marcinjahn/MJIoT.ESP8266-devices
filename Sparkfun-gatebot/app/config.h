14cec4353e860b25eccbde89b2da0f710ee2c03c
Marcin <marcinj10@gmail.com>
2018-08-31 21:33:53 +0200

    Add GateBot device

---
+++ Sparkfun-gatebot/app/config.h
@@ -0,0 +1,17 @@
+// Physical device information for board and sensor
+#define DEVICE_ID "46"
+
+// Pin layout configuration
+#define SERVO_PIN 2
+#define SENSOR_PIN 5
+
+// EEPROM address configuration
+#define EEPROM_SIZE 512
+
+// SSID and SSID password's length should < 32 bytes
+// http://serverfault.com/a/45509
+#define SSID_LEN 32
+#define PASS_LEN 32
+#define CONNECTION_STRING_LEN 256
+
+#define MESSAGE_MAX_LEN 256
---
+++ Sparkfun-gatebot/app/message.ino
@@ -0,0 +1,25 @@
+#include <ArduinoJson.h>
+
+bool createServoMessage(char *payload, int value)
+{
+    StaticJsonBuffer<MESSAGE_MAX_LEN> jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+    
+    root["DeviceId"] = DEVICE_ID;
+    root["PropertyName"] = "Servo Position";
+    root["PropertyValue"] = value;
+    
+    root.printTo(payload, MESSAGE_MAX_LEN);
+}
+
+bool createSensorMessage(char *payload, bool value)
+{
+    StaticJsonBuffer<MESSAGE_MAX_LEN> jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+    
+    root["DeviceId"] = DEVICE_ID;
+    root["PropertyName"] = "Light State";
+    root["PropertyValue"] = value;
+    
+    root.printTo(payload, MESSAGE_MAX_LEN);
+}
